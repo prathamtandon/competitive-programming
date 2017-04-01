67a840cf084b6e78e55395a8882d062b646f6e0e
Pratham Tandon <prathamt@outlook.com>
2017-04-01 22:25:33 +0530

    Refactor

--- UVa/Q272.java
+++ UVa/Q272.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
+class Q272 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         TexTransform tt = new TexTransform();
